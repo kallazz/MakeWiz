c24230dbd06850f8e691e5c7f77bc814be3e5bad
kallazz <michael.w.kallas@gmail.com>
2023-08-17 12:11:24 +0200

    Set a fixed path

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (directory_path, executable_file) = parse_arguments(&args).unwrap_or_else(|err| {
+    let executable_name = parse_arguments(&args).unwrap_or_else(|err| {
         eprintln!("Error: {}", err);
         process::exit(1);
     });
 
-    let paths_to_files = fs::read_dir(directory_path).unwrap_or_else(|err| {
+    let paths_to_files = fs::read_dir(".").unwrap_or_else(|err| {
         eprintln!("Error: {}", err);
         process::exit(1);
     });
@@ -21,16 +21,15 @@ fn main() {
     });
 }
 
-fn parse_arguments(args: &Vec<String>) -> Result<(&str, &str), &str> {
-    if args.len() < 3 {
+fn parse_arguments(args: &Vec<String>) -> Result<&str, &str> {
+    if args.len() < 2 {
         return Err("Not enough arguments");
     }
     //Flags will be added later
 
-    let directory_path = &args[1];
-    let executable_file = &args[2];
+    let executable_name = &args[1];
 
-    Ok((directory_path, executable_file))
+    Ok(executable_name)
 }
 
 fn paths_to_file_names(file_paths: fs::ReadDir) -> Result<Vec<String>, Box<dyn std::error::Error>>{
