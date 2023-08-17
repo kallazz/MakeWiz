1f8b7bf3bcd6abe4b0fd8fe0257f8272e1a763fc
kallazz <michael.w.kallas@gmail.com>
2023-08-17 19:40:05 +0200

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -36,3 +36,60 @@ fn check_extension(name: &fs::DirEntry) -> bool {
         None => false
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //Testing parse_arguments()
+    #[test]
+    fn parse_arguments_no_arguments() {
+        let args: Vec<String> = vec![String::from("target/debug/genmake")];
+        assert_eq!(Err("Not enough arguments"), parse_arguments(&args));
+    }
+
+    #[test]
+    fn parse_arguments_one_argument() {
+        let args: Vec<String> = vec![String::from("target/debug/genmake"), String::from("filename")];
+        assert_eq!(Ok("filename"), parse_arguments(&args));
+    }
+
+    //Testing extract_names()
+    #[test]
+    fn extract_names_no_files() {
+        let paths = fs::read_dir("./test-dirs/empty").unwrap();
+        let expected_vec: Vec<String> = vec![];
+        let result_vec = extract_names(paths).unwrap();
+
+        assert_eq!(expected_vec, result_vec);
+    }
+
+    #[test]
+    fn extract_names_no_correct_files() {
+        let paths = fs::read_dir("./test-dirs/no-correct-files").unwrap();
+        let expected_vec: Vec<String> = vec![];
+        let result_vec = extract_names(paths).unwrap();
+
+        assert_eq!(expected_vec, result_vec);
+    }
+
+    #[test]
+    fn extract_names_correct_files_without_folders() {
+        let paths = fs::read_dir("./test-dirs/standard-with-folders").unwrap();
+        let expected_vec: Vec<String> = vec![String::from("cpp_header.hpp"), 
+            String::from("c_header.h"), String::from("cpp_source.cpp"), String::from("c_source.c")];
+        let result_vec = extract_names(paths).unwrap();
+
+        assert_eq!(expected_vec, result_vec);
+    }
+
+    #[test]
+    fn extract_names_correct_files_with_folders() {
+        let paths = fs::read_dir("./test-dirs/standard-with-folders").unwrap();
+        let expected_vec: Vec<String> = vec![String::from("cpp_header.hpp"), 
+            String::from("c_header.h"), String::from("cpp_source.cpp"), String::from("c_source.c")];
+        let result_vec = extract_names(paths).unwrap();
+
+        assert_eq!(expected_vec, result_vec);
+    }
+}
